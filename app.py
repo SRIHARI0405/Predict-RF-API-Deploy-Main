7a27573edd56e0e374817d83b411081d2d8819ba
user <administrator@Srihari.local>
2024-01-25 17:42:39 +0530

    commited Done

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def get_profile_route(username):
             follower_count_value = user_info.follower_count
             followers_data = []
             followers_data1 = []
-            followers = cl.user_followers(user_id, amount=2)
+            followers = cl.user_followers(user_id, amount=30)
             for follower_id in followers:
               try:
                 follower_info = cl.user_info(follower_id)
@@ -71,8 +71,8 @@ def get_profile_route(username):
 
             follower_data_count = len(followers_data)
             random_profile = 0
-            if follower_data_count > 1:
-                random_profile = 1
+            if follower_data_count > 20:
+                random_profile = 20
             else:
                 random_profile = follower_data_count
             selected_followers = random.sample(followers_data, random_profile)
